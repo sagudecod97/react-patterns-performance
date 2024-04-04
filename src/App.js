592a8e695ddd705315265fb1f54e595aae024056
Santiago Agudelo <sagudecod97@gmail.com>
2024-04-04 12:44:57 -0500

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import "./App.scss";
-import { authors } from "./data/authors";
-
-import Modal from "./components/modal/modal";
-import AuthorsList from "./components/lists/authors-list/authors-list";
-import ListsComponent from "./components/lists/lists-component";
 
 function App() {
-  return (
-    <div className="App">
-      <Modal buttonText="Open modal">
-        <ListsComponent
-          items={authors}
-          sourcePropName="authors"
-          ListToRender={AuthorsList}
-        />
-      </Modal>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
