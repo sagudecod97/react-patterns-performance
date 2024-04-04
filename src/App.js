e2e8a5686daf2e1592bb502db695a0023be1b821
Santiago Agudelo <sagudecod97@gmail.com>
2024-04-04 12:14:13 -0500

    Erased App content

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import "./App.scss";
-import { authors } from "./data/authors";
-import { books } from "./data/books";
-
-import AuthorsList from "./components/lists/authors-list/authors-list";
-import BooksList from "./components/lists/books-list/books-list";
-import ListsComponent from "./components/lists/lists-component";
 
 function App() {
-  return (
-    <div className="App">
-      <ListsComponent
-        items={authors}
-        sourcePropName="authors"
-        ListToRender={AuthorsList}
-      />
-
-      <ListsComponent
-        items={books}
-        sourcePropName="books"
-        ListToRender={BooksList}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
